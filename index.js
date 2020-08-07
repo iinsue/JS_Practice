81cca4466b53037d72f4905d45afdab99c0b4db7
iinsue <hinsu93@naver.com>
2020-08-07 15:33:14 +0900

    remaining time

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ function getTime() {
   }`;
 }
 
+function leftTime() {
+  const printTime = document.querySelector("#xmas");
+  const xmas = new Date("2020-12-24:00:00:00+0900");
+  const now = new Date();
+  let leftDay = xmas - now;
+  leftDay = Math.floor(leftDay / 1000); // milisec
+  const seconds = leftDay % 60;
+  leftDay = Math.floor(leftDay / 60); // minute
+  const minutes = leftDay % 60;
+  leftDay = Math.floor(leftDay / 60); // hour
+  const hours = leftDay % 24;
+  leftDay = Math.floor(leftDay / 24);
+  const days = leftDay;
+
+  printTime.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
 function init() {
   setInterval(getTime, 1000);
+  setInterval(leftTime, 1000);
 }
 
 init();
